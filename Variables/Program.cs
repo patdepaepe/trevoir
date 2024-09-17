1063bc7285c04fd4eaff79c234c9a2058f9480f0
patrick de paepe <patjedepaepe1976@gmail.com>
2024-09-17 16:16:45 +0200

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name = string.Empty;
+int age = 0;
+int retirementAge = 65;
+
+
+Console.Write("Enter your name: ");
+name = Console.ReadLine();
+
+Console.Write("Enter your age: ");
+age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Full Name: {name}");
+Console.WriteLine($"Age: {age}");
+int workingYearsLeft = retirementAge - age;
+Console.WriteLine($"Working Years remaining: {workingYearsLeft}");
+
+
---
+++ introduction/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello Patje");
+Console.Write("Hello Patikootje");
+Console.WriteLine("Hello Patje2");
+
+Console.ReadKey();
