35512bbdc3a8c55a885b9bd6002fdf6af3f8cd48
patrick de paepe <patjedepaepe1976@gmail.com>
2024-09-17 20:32:52 +0200

    operators

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,18 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string name = string.Empty;
-int age = 0;
+string firstName = string.Empty;
+string lastName = string.Empty;
+int age;
 int retirementAge = 65;
+decimal salary;
+char gender = char.MinValue;
+bool working = true;
 
 
-Console.Write("Enter your name: ");
-name = Console.ReadLine();
+Console.Write("Enter your first name: ");
+firstName = Console.ReadLine();
+
+Console.Write("Enter your last name: ");
+lastName = Console.ReadLine();
 
 Console.Write("Enter your age: ");
 age = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"Full Name: {name}");
+Console.Write("Enter your salary: ");
+salary = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Enter your gender(m or f): ");
+gender = Convert.ToChar(Console.ReadLine());
+
+Console.Write("Are you employed (true olr false):  ");
+working = Convert.ToBoolean(Console.ReadLine());
+
+Console.WriteLine($"Full Name: {firstName} {lastName}");
 Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Your salary is:{salary}");
+Console.WriteLine($"Yiur gender is: {gender}");
+Console.WriteLine($"Are you working: {working}");
 int workingYearsLeft = retirementAge - age;
 Console.WriteLine($"Working Years remaining: {workingYearsLeft}");
 
---
+++ operators/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Please enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int num2;
+Console.Write("Enter second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+int sum = num1 + num2;
+
+Console.WriteLine(sum);
+
+
+Console.WriteLine();
