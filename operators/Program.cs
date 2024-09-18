64b8edf5731b580eda22a41f1678430bd51c12e7
patrick de paepe <patjedepaepe1976@gmail.com>
2024-09-18 12:14:28 +0200

    calculator

--- operators/Program.cs
+++ operators/Program.cs
@@ -1,14 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Please enter the first number: ");
+Console.WriteLine("Welcome to the sample calculator");
+
+Console.Write("please enter the first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
-int num2;
-Console.Write("Enter second number: ");
-num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-int sum = num1 + num2;
+Console.WriteLine("Please select an operation: ");
+Console.WriteLine("1. Addition");
+Console.WriteLine("2. Subtraction");
+Console.WriteLine("3. Multiplication");
+Console.WriteLine("4. Division");
+int choice = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(sum);
+int result = 0;
+switch (choice)
+{
+    case 1:
+        result = num1 + num2;
+        break;
 
+    case 2:
+        result = num1 - num2;
+        break;
 
-Console.WriteLine();
+    case 3:
+        result = num1 * num2;
+        break;
+    case 4:
+        result = num1 / num2;
+        break;
+    default:
+        Console.WriteLine("Invalid Choice");
+        break;
+
+     
+
+}
+
+Console.WriteLine($"The result is {result}");
+Console.ReadLine();
